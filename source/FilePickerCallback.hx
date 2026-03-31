2c67a3ec86c357457aa249ff391a3dba0a4aeae7
Ethantobot <ethantobot7@gmail.com>
2026-03-31 15:25:25 +0200

    Create FilePickerCallback.hx

---
+++ source/FilePickerCallback.hx
@@ -0,0 +1,24 @@
+import sys.io.File;
+import lime.system.System;
+
+class FilePickerCallback {
+	public static var onPicked: String->Void;
+
+	public static function onFilePicked(path:String):Void {
+		trace("Picked: " + path);
+
+		// Copy to safe location (IMPORTANT for iOS)
+		var fileName = path.split("/").pop();
+		var newPath = System.applicationStorageDirectory + "/" + fileName;
+
+		try {
+			File.copy(path, newPath);
+		} catch(e) {
+			trace("Copy failed: " + e);
+		}
+
+		if (onPicked != null) {
+			onPicked(newPath);
+		}
+	}
+}
