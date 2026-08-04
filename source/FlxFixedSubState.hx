557323e03d5a281d60b62e407e046000ab8c577a
Ethantobot <ethantobot7@gmail.com>
2026-08-04 19:59:17 +0200

    Refactor FlxFixedSubState to use conditional imports

--- source/FlxFixedSubState.hx
+++ source/FlxFixedSubState.hx
@@ -1,11 +1,23 @@
 package;
 
-class FlxFixedSubState extends FlxSubState {
+#if haxe3ds
+import citro.state.CitroSubState;
+typedef BaseFixedSubState = CitroSubState;
+#else
+import flixel.FlxSubState;
+import flixel.FlxBasic;
+typedef BaseFixedSubState = FlxFixedSubState;
+#end
+
+class FlxFixedSubState extends BaseFixedSubState {
+	#if !haxe3ds
 	override function add(basic:FlxBasic):FlxBasic {
 		super.add(basic);
 		if (cameras != null) basic.cameras = cameras;
 		return basic;
 	}
+	#end
+
 	function new() {
 		super();
 	}
