df052012b0180fdbc60ab4091a9e2f276030b3ef
Ethantobot <ethantobot7@gmail.com>
2026-08-04 23:39:28 +0200

    Add files via upload

--- convert_assets.sh
+++ convert_assets.sh
@@ -7,23 +7,23 @@ touch assets/romfs/haxe3ds/version
 export PATH=/opt/devkitpro/tools/bin:$PATH
 
 if [ -d "assets" ]; then
-  for file in $(find assets/ -type f -name "*.png"); do
-    dir=$(dirname "$file")
-    filename=$(basename "$file")
-    nameonly="${filename%.*}"
-    
-    echo "Converting $file to T3X..."
-    tex3ds -i "$file" -o "${dir}/${nameonly}.t3x"
-  done
+    for file in $(find assets/ -type f -name "*.png"); do
+        dir=$(dirname "$file")
+        filename=$(basename "$file")
+        nameonly="${filename%.*}"
+        
+        echo "Converting $file to T3X..."
+        tex3ds -i "$file" -o "${dir}/${nameonly}.t3x"
+    done
 
-  for xmlfile in $(find assets/ -type f -name "*.xml"); do
-    dir=$(dirname "$xmlfile")
-    filename=$(basename "$xmlfile")
-    nameonly="${filename%.*}"
-    ceafile="${dir}/${nameonly}.cea"
-    
-    echo "Starting converting $xmlfile to $ceafile..."
-    python3 - <<EOF
+    for xmlfile in $(find assets/ -type f -name "*.xml"); do
+        dir=$(dirname "$xmlfile")
+        filename=$(basename "$xmlfile")
+        nameonly="${filename%.*}"
+        ceafile="${dir}/${nameonly}.cea"
+        
+        echo "Converting $xmlfile to $ceafile..."
+        python3 - <<EOF
 import xml.etree.ElementTree as ET
 import re
 
@@ -47,22 +47,22 @@ try:
 except Exception as e:
     print(f"Error parsing $xmlfile: {e}")
 EOF
-  done
+    done
 
-  for file in $(find assets/ -type f -name "*.mp3"); do
-    dir=$(dirname "$file")
-    filename=$(basename "$file")
-    nameonly="${filename%.*}"
-    ffmpeg -y -i "$file" -q:a 4 "${dir}/${nameonly}.ogg"
-    rm "$file"
-  done
+    for file in $(find assets/ -type f -name "*.mp3"); do
+        dir=$(dirname "$file")
+        filename=$(basename "$file")
+        nameonly="${filename%.*}"
+        ffmpeg -y -i "$file" -q:a 4 "${dir}/${nameonly}.ogg"
+        rm "$file"
+    done
 
-  for file in $(find assets/ -type f -name "*.ogg"); do
-    dir=$(dirname "$file")
-    filename=$(basename "$file")
-    nameonly="${filename%.*}"
-    
-    echo "Converting $file to CWAV..."
-    cwavtool -i "$file" -o "${dir}/${nameonly}.cwav"
-  done
+    for file in $(find assets/ -type f -name "*.ogg"); do
+        dir=$(dirname "$file")
+        filename=$(basename "$file")
+        nameonly="${filename%.*}"
+        
+        echo "Converting $file to CWAV..."
+        cwavtool -i "$file" -o "${dir}/${nameonly}.cwav"
+    done
 fi
