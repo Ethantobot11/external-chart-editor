694cf8e80c3e345c7092efe23eb8d91cfd6284c0
Ethantobot <ethantobot7@gmail.com>
2026-08-04 20:27:10 +0200

    Update FPSCounter.hx

--- source/debug/FPSCounter.hx
+++ source/debug/FPSCounter.hx
@@ -1,14 +1,20 @@
 package debug;
 
+#if !haxe3ds
 import flixel.FlxG;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 import openfl.system.System as OpenFlSystem;
 import lime.system.System as LimeSystem;
+#else
+import citro.backend.CitroColor;
+import citro.object.CitroText;
+import haxe3ds.Env;
+#end
 
 /**
 	The FPS class provides an easy-to-use monitor to display
-	the current frame rate of an OpenFL project
+	the current frame rate of an OpenFL project or haxe3ds platform
 **/
 #if cpp
 #if windows
@@ -19,7 +25,7 @@ import lime.system.System as LimeSystem;
 @:headerInclude('sys/utsname.h')
 #end
 #end
-class FPSCounter extends TextField
+class FPSCounter extends #if !haxe3ds TextField #else CitroText #end
 {
 	/**
 		The current frame rate, expressed using frames-per-second
@@ -28,7 +34,7 @@ class FPSCounter extends TextField
 	public static var currentFPSStatic(default, null):Int;
 
 	/**
-		The current memory usage (WARNING: this is NOT your total program memory usage, rather it shows the garbage collector memory)
+		The current memory usage
 	**/
 	public static var memoryMegas(get, never):Float;
 
@@ -38,6 +44,7 @@ class FPSCounter extends TextField
 
 	public function new(x:Float = 10, y:Float = 10, color:Int = 0x000000)
 	{
+		#if !haxe3ds
 		super();
 
 		if (LimeSystem.platformName == LimeSystem.platformVersion || LimeSystem.platformVersion == null)
@@ -55,19 +62,28 @@ class FPSCounter extends TextField
 		width = FlxG.width;
 		multiline = true;
 		text = "FPS: ";
+		#else
+		super(x, y, "FPS: ");
+		var modeStr = Env.is3DSX ? "3DSX" : "CIA";
+		os = '\nOS: Nintendo 3DS (ARM11) [$modeStr]';
+		currentFPS = 0;
+		currentFPSStatic = 0;
+		this.color = 0xFFFFFFFF;
+		setBorderStyle(0xFF000000, 1, OUTLINE);
+		#end
 
 		times = [];
 	}
 
 	var deltaTimeout:Float = 0.0;
 
-	// Event Handlers
+	#if !haxe3ds
 	private override function __enterFrame(deltaTime:Float):Void
 	{
 		final now:Float = haxe.Timer.stamp() * 1000;
 		times.push(now);
 		while (times[0] < now - 1000) times.shift();
-		// prevents the overlay from updating every frame, why would you need to anyways @crowplexus
+
 		if (deltaTimeout < 50) {
 			deltaTimeout += deltaTime;
 			return;
@@ -78,9 +94,29 @@ class FPSCounter extends TextField
 		updateText();
 		deltaTimeout = 0.0;
 	}
+	#else
+	override function update(delta:Int):Void
+	{
+		super.update(delta);
+		final now:Float = haxe.Timer.stamp() * 1000;
+		times.push(now);
+		while (times[0] < now - 1000) times.shift();
+
+		if (deltaTimeout < 50) {
+			deltaTimeout += delta;
+			return;
+		}
+
+		currentFPS = times.length < 60 ? times.length : 60;
+		currentFPSStatic = currentFPS;
+		updateText();
+		deltaTimeout = 0.0;
+	}
+	#end
 
 	public dynamic function updateText():Void
 	{
+		#if !haxe3ds
 		text = 
 		'FPS: $currentFPS' +
 		'\nChart Editor v0.0.1' +
@@ -90,15 +126,30 @@ class FPSCounter extends TextField
 		textColor = 0xFFFFFFFF;
 		if (currentFPS < FlxG.drawFramerate * 0.5)
 			textColor = 0xFFFF0000;
+		#else
+		text = 'FPS: $currentFPS\nChart Editor v0.0.1$os';
+		color = (currentFPS < 30) ? 0xFFFF0000 : 0xFFFFFFFF;
+		#end
 	}
 
 	inline static function get_memoryMegas():Float
+	{
+		#if (cpp && !haxe3ds)
 		return cpp.vm.Gc.memInfo64(cpp.vm.Gc.MEM_INFO_USAGE);
+		#else
+		return 0.0;
+		#end
+	}
 
 	public inline function positionFPS(X:Float, Y:Float, ?scale:Float = 1){
+		#if !haxe3ds
 		scaleX = scaleY = #if android (scale > 1 ? scale : 1) #else (scale < 1 ? scale : 1) #end;
 		x = FlxG.game.x + X;
 		y = FlxG.game.y + Y;
+		#else
+		x = X;
+		y = Y;
+		#end
 	}
 
 	#if cpp
