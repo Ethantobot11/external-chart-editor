1004f9305a0417e81b1642cacce72c3080c64fb2
Ethantobot <ethantobot7@gmail.com>
2026-08-04 05:41:21 +0200

    Create Deque.hx

---
+++ source/std/sys/thread/Deque.hx
@@ -0,0 +1,10 @@
+package sys.thread;
+
+class Deque<T> {
+    public function new():Void {}
+    public function add(i:T):Void {}
+    public function push(i:T):Void {}
+    public function pop(block:Bool):Null<T> {
+        return null;
+    }
+}
